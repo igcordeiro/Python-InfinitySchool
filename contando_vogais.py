6c40ee1c65cd73942e13efb091f96b8addb11a17
Igor <55407170+igcordeiro@users.noreply.github.com>
2022-07-14 08:26:40 -0300

    Update contando_vogais.py

--- contando_vogais.py
+++ contando_vogais.py
@@ -1,12 +1,21 @@
-###################################################################### CONTANDO VOGAIS EM DOIS NOMES
+###################################################################### CONTANDO VOGAIS EM UM NOMES
 
-def count_vogais(nome1,nome2):
+def count_vogais(nomes):
     qtd_vogais=0
-    total=str(nome1+nome2)
-    for letras in (total):
+    nomes=str(nomes)
+    for letras in (nomes):
         if letras in 'aeiouAEIOU':
             qtd_vogais+=1
-    print(f'A quantidade de vogais em {nome1} e em {nome2} é igual a {qtd_vogais}!\n')
-pessoa1=str(input('Digite o nome da pessoa1 para que se conte as vogais de seu nome:\n'))
-pessoa2=str(input('Digite o nome da pessoa2 para que se conte as vogais de seu nome:\n'))
-count_vogais(pessoa1,pessoa2)
+    print(f'A quantidade de vogais de todos os nomes digitados é igual a {qtd_vogais}!\n')
+lista=[]
+while True:
+    resposta=input("Deseja cadastrar alguém para contar o número de vogais em seu nome?\n")
+    pessoas={}
+    if resposta in "sSsimSIMSimsIM":
+        nome=str(input('Digite um nome:\n'))
+        pessoas['N@m3']=nome
+        lista.append(pessoas)
+        continue
+    else:
+        break
+count_vogais(lista)
